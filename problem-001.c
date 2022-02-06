3c930f9adc27d374787c679e510803b63bb7f306
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-02-06 18:44:55 +0100

    Simplified solution 1 and added correct numbers

--- problem-001.c
+++ problem-001.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    size_t t = 3;
+    int N = 1000, p = 0, sum = 0;
 
-    while (t--) {
-        size_t N = 10, p = 0, sum = 0;
-
-        for (size_t index = 0; index < N; index++) {
-            if (index % 3 == 0 || index % 5 == 0) {
-                sum = sum + index;
-            }
+    for (int index = 0; index < N; index++) {
+        if (index % 3 == 0 || index % 5 == 0) {
+            sum = sum + index;
         }
-
-        printf("%ld\n", sum);
     }
+
+    printf("%d\n", sum);
 
     return 0;
 }
