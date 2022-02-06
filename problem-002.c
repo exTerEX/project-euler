44b02604190b8a24fe8086f97f32c707c36cffde
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-02-06 18:53:30 +0100

    Fixed and simplified solution 2

--- problem-002.c
+++ problem-002.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
-    size_t n = 10, sum = 0, index = 1, jndex = 2, temp;
+    uint32_t x1 = 0, x2 = 1, x3 = 2, sum;
 
-    while (jndex <= n) {
-        if ((jndex & 1) == 0) {
-            sum += jndex;
-        }
-
-        temp = index;
-        index = jndex;
-        jndex = temp + index;
+    while (x3 <= 4000000) {
+        x3 = x1 + x2;
+        sum += x3 * !(x3 % 2);
+        x1 = x2;
+        x2 = x3;
     }
 
-    printf("%ld\n", sum);
+    printf("%u\n", sum);
 
     return 0;
 }
